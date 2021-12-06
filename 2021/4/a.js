537a4f7c6c3b820c39dc89d7c237f07c4887b1ea
Mallory Mollo <mallory.mollo@buyco.co>
2021-12-06 17:10:01 +0100

    Day 4

---
+++ 2021/4/a.js
@@ -0,0 +1,109 @@
+const { readFileSync } = require('fs')
+
+
+
+input = readFileSync('./input').toString('ascii').split("\r\n\r\n")
+
+seq = input.shift().split(',').map(x => parseInt(x))
+
+Cell = function(value, row, col, marked = false) {
+	this.value = value
+	this.row = row
+	this.col = col
+	this.marked = marked
+}
+
+Board = function (data, size = 5) {
+	this.cells = new Map()
+	this.rows = new Map()
+	this.cols = new Map()
+	
+	let i = 0;
+	data.forEach(n => {
+		row = Math.floor(i/size)
+		col = i % size
+		cell = new Cell(n, row, col)
+		this.cells.set(n, cell)
+		if(!this.rows.has(row)) {
+			this.rows.set(row, [])
+		}
+		
+		if(!this.cols.has(col)) {
+			this.cols.set(col, [])
+		}
+		this.rows.get(row).push(cell)
+		this.cols.get(col).push(cell)
+		i++;
+	})
+	
+	
+}
+
+Board.prototype.mark = function(n) {
+	let cell = this.cells.get(n)
+	if(cell === undefined) return
+	cell.marked = true
+	return cell
+}
+
+Board.prototype.score = function() {
+	s = 0
+	this.cells.forEach(c => {
+		if(c.marked) return
+		s+= c.value
+	})
+	return s
+}
+
+// Part 1
+function a(input) {
+	boards = new Set(input.map( b => {
+		let data = b.replace(/\r\n/g, ' ').replace(/ +/g, ' ').trim().split(' ').map(x => parseInt(x))
+		return new Board(data)
+	}))
+	let n, b
+	seq:
+	for(n of seq) {
+		boards:
+		for(b of Array.from(boards.values())) {
+			let cell = b.mark(n)
+			if(cell === undefined) continue
+			
+			if(b.rows.get(cell.row).every(c => c.marked) || b.cols.get(cell.col).every(c => c.marked)) break seq
+		}
+	}
+	return n * b.score()
+}
+
+
+// Part 2
+function b(input) {
+	boards = new Set(input.map( b => {
+		let data = b.replace(/\r\n/g, ' ').replace(/ +/g, ' ').trim().split(' ').map(x => parseInt(x))
+		return new Board(data)
+	}))
+	let n, b
+	seq:
+	for(n of seq) {
+		boards:
+		for(b of Array.from(boards.values())) {
+			let cell = b.mark(n)
+			if(cell === undefined) continue
+			
+			if(b.rows.get(cell.row).every(c => c.marked) || b.cols.get(cell.col).every(c => c.marked)) {
+				if(boards.size === 1) {
+					break seq
+				}
+				boards.delete(b)
+			}
+		}
+	}
+	
+	b = Array.from(boards.values()).pop()
+
+	return n * b.score()
+	
+}
+
+console.log(a(input))
+console.log(b(input))
