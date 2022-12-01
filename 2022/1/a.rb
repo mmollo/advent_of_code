5a7698f34c4aa02ad51906387a8de09900333375
Mallory Mollo <mallory.mollo@gmail.com>
2022-12-01 09:47:55 +0100

    2022 - Day 1

---
+++ 2022/1/a.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+def input
+  File.read('input')
+      .split("\n\n")
+      .map(&:split)
+      .map { |elve| elve.map(&:to_i).sum }
+end
+
+p input.max
+
+p input.sort.max(3).sum
